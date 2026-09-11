4ab8b4116cc222148ac2961a146ac97b344eac70
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-09-11 19:35:38 +0200

    feat(event): add event.hpp who defined event class and got 2 array for stocking if the Key are pressed or realeasd,an enum Key and fwd file

---
+++ src/fwd.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/*Forward Declaration*/
+
+#include <cstdint>
+
+namespace rtk 
+{
+    class Window;
+
+    class Event;
+    enum class Key : uint16_t;
+}
--- test/main.cpp
+++ test/main.cpp
@@ -3,10 +3,14 @@
 #include "../src/texture/textureManager.hpp"
 #include "../src/render/SpriteRenderer.hpp"
 
+#include "../sprite/sprite.hpp"
+
 #include <time.h>
 #include <unistd.h>
 
 /*Test Open Window*/
+
+
 int main()
 {
     rtk::Window window(1920, 1080, "Vulkan");
@@ -16,10 +20,16 @@ int main()
 
     uint32_t testTexId = textureManager.loadTexture("texture.png");
 
+
+    rtk::SpriteData sprite;
+
+    sprite.position = {1000.0f, 0.0f};
+    
+
     while (window.pollEvents()) {
         spriteRenderer.beginFrame();
-        spriteRenderer.drawSprite({100.0f, 000.0f}, {1000.0f, 1000.0f}, 0.0f, testTexId);
-
+        spriteRenderer.drawSprite({100.0f, 0.0f}, {1000.0f, 1000.0f}, 0.0f, testTexId);
+        spriteRenderer.drawSprite(glm::vec2(sprite.position.x, sprite.position.y), {100.0f, 100.0f}, 0.0f, testTexId);
         spriteRenderer.endFrame();
     }
 
