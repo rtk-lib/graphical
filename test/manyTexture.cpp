faf7fcd67ca27440f11739d595c7e1ae386d977a
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-09-21 17:36:00 +0200

    fix(crossPlateform): stop using unistd

--- test/manyTexture.cpp
+++ test/manyTexture.cpp
@@ -2,8 +2,7 @@
 #include "../include/sprite/sprite.hpp"
 #include "../include/Logger/Logger.hpp"
 
-#include <time.h>
-#include <unistd.h>
+#include <cstdint>
 
 int main()
 {
@@ -15,19 +14,20 @@ int main()
     rtk::Sprite sonicSprite(sonicTex);
     rtk::Sprite marioSprite(marioTex);
 
-    sonicSprite.setPosition({500.0f, 400.0f});
+    float sonicX = 500.0f;
+    float sonicY = 400.0f;
+    sonicSprite.setPosition({sonicX, sonicY});
     sonicSprite.setSize({300.0f, 300.0f});
     sonicSprite.setRotation(0.0f);
 
-    uint16_t moveX = 0;
+    std::uint16_t moveX = 0;
     sonicSprite.setTextureRect({moveX, 0, 607, 794});
+
     float x = 1100.0f;
     marioSprite.setPosition({x, 400.0f});
     marioSprite.setSize({300.0f, 300.0f});
     marioSprite.setRotation(0.0f);
-
     marioSprite.setFlipX(true);
-
 
     rtk::Event rtkEvent;
     rtk::RGB clearColor = {100, 100, 100};
@@ -36,15 +36,25 @@ int main()
         if (!rWindow.beginFrame(clearColor))
             continue;
 
-        if (rtkEvent.isKeyPressed(rtk::Key::A)){
+        float speed = 5.0f;
+
+        if (rtkEvent.isKeyPressed(rtk::Key::Z)) sonicY -= speed;
+        if (rtkEvent.isKeyPressed(rtk::Key::S)) sonicY += speed;
+        if (rtkEvent.isKeyPressed(rtk::Key::Q)) sonicX -= speed;
+        if (rtkEvent.isKeyPressed(rtk::Key::D)) sonicX += speed;
+
+        sonicSprite.setPosition({sonicX, sonicY});
+
+        if (rtkEvent.isKeyPressed(rtk::Key::A)) {
             x++;
             moveX++;
+
             marioSprite.setPosition({x, 400.0f});
             sonicSprite.setTextureRect({moveX, 0, 607, 794});
         }
 
-        rWindow.drawSprite(sonicSprite);
         rWindow.drawSprite(marioSprite);
+        rWindow.drawSprite(sonicSprite);
 
         rWindow.endFrame();
     }
